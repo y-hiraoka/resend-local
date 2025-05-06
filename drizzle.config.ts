0780ae0cf9aeed369e5b8be76dd14443bc41c70d
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2025-05-06 22:14:59 +0900

    from Next.js to vite

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
   out: "./drizzle",
-  schema: "./src/database/schema.ts",
+  schema: "./src/server/database/schema.ts",
   dialect: "sqlite",
   dbCredentials: {
     url: process.env.DB_FILE_NAME!,
