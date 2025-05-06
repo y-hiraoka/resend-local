1fa9a0355e50cd4379947e78feffbae134cfe03a
y-hiraoka <62982380+y-hiraoka@users.noreply.github.com>
2025-05-07 01:34:33 +0900

    install hono

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    outDir: "dist/client",
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
